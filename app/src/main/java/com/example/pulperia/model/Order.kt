162cae9ce60071526542771d3b4fb1cc70c0c18f
hbodan <hbodan@uamv.edu.ni>
2026-04-15 17:49:17 -0600

    Order entity added

--- app/src/main/java/com/example/pulperia/model/Order.kt
+++ app/src/main/java/com/example/pulperia/model/Order.kt
@@ -1,2 +1,9 @@
 package com.example.pulperia.model
 
+// Modelo de datos simple para representar el pedido
+data class Order(
+    val id: Long = System.currentTimeMillis(),
+    val clientName: String,
+    val productName: String,
+    val quantity: String
+)
