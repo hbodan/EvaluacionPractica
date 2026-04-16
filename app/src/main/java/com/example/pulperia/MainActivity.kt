8193b0fe4902d187968561e54e65cf1b28e01bd2
AndreEJA <aejoiner@uamv.edu.ni>
2026-04-15 18:12:08 -0600

    main listo

--- app/src/main/java/com/example/pulperia/MainActivity.kt
+++ app/src/main/java/com/example/pulperia/MainActivity.kt
@@ -3,45 +3,20 @@ package com.example.pulperia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pulperia.ui.theme.PulperiaTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.tuempresa.pulperia.ui.screens.OrderScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            PulperiaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            // Usamos un tema limpio y cargamos nuestra pantalla principal
+            MaterialTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    OrderScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PulperiaTheme {
-        Greeting("Android")
     }
 }
